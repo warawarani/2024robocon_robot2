52756b4f6db4e907138e7ccf4efe1124c6fc63ce
warawarani <warawarani6201@gmail.com>
2024-05-21 19:01:59 +0900

    エンコーダーの内部プルアップをonにした

--- Core/Src/stm32f3xx_hal_msp.c
+++ Core/Src/stm32f3xx_hal_msp.c
@@ -103,7 +103,7 @@ void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* htim_encoder)
     */
     GPIO_InitStruct.Pin = ENCODER_A_Pin|ENCODER_B_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF6_TIM1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
